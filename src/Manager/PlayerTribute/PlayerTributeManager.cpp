e920db98e0ab07f062ebf89c500d573790d74669
pessifuff <pessimistic.main@gmail.com>
2023-12-20 00:27:55 +0300

    Updated & fixed player tribute

--- src/Manager/PlayerTribute/PlayerTributeManager.cpp
+++ src/Manager/PlayerTribute/PlayerTributeManager.cpp
@@ -6,30 +6,38 @@ PlayerTributeManager* GetPlayerTributeManager() {
 }
 
 void PlayerTributeManager::Build() {
-    std::string playerTribute {
+    std::string epic_players {
         fmt::format(
-            "`oDeveloper: `psix\n"
+            "`oDeveloper: `psix``"
         )
     };
 
-    std::string mentorTribute {
+    std::string exceptional_mentors {
 		fmt::format(
-			"`oTest\n"
+			"`oTest``"
 		)
 	};
 
-    m_size = sizeof(uint32_t) + playerTribute.length() + mentorTribute.length();
+    std::string charity_champions {
+        fmt::format(
+            "`oTest``"
+        )
+    };
+
+    m_size = sizeof(uint32_t) + epic_players.length() + exceptional_mentors.length() + charity_champions.length();
     m_data = static_cast<char*>(std::malloc(m_size));
 
     BinaryWriter writer(reinterpret_cast<uint8_t*>(m_data));
-    writer.Write<uint16_t>(static_cast<uint16_t>(playerTribute.length()));
-    writer.Write(playerTribute);
-    writer.Write<uint16_t>(static_cast<uint16_t>(mentorTribute.length()));
-    writer.Write(mentorTribute);
+    writer.Write<uint16_t>(static_cast<uint16_t>(epic_players.length()));
+    writer.Write(epic_players.c_str(), epic_players.length());
+    writer.Write<uint16_t>(static_cast<uint16_t>(exceptional_mentors.length()));
+    writer.Write(exceptional_mentors.c_str(), exceptional_mentors.length());
+    writer.Write<uint16_t>(static_cast<uint16_t>(charity_champions.length()));
+    writer.Write(charity_champions.c_str(), charity_champions.length());
 
     m_hash = Utils::GetProtonHash(writer.Get(), m_size);
 
-    Logger::Print(INFO, "PlayerTributeManager - Hash: {} | Data: {}", m_hash, sizeof(m_data));
+    Logger::Print(INFO, "PlayerTributeManager Built - Hash: {} | Data: {}", m_hash, sizeof(m_data));
 }
 
 void PlayerTributeManager::Kill() {
